63af64c38c20fba1cf701d09094f1e25ba5d145e
hukunling <1746530669@qq.com>
2020-12-06 16:53:23 +0800

    初始化项目

---
+++ gulu-ui/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+ <div>
+   hi
+ </div>
+ <hkl/>
+</template>
+
+<script>
+import hkl from './components/hkl.vue'
+
+export default {
+  name: 'App',
+  components: {
+    hkl
+  }
+}
+</script>
