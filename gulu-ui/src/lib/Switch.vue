53ce6cf6a6c8fea3d82f0c5f33f2a1156bbd55bf
hukunling <1746530669@qq.com>
2020-12-14 15:07:42 +0800

    使用css切换内容

--- gulu-ui/src/lib/Switch.vue
+++ gulu-ui/src/lib/Switch.vue
@@ -27,18 +27,7 @@ $h2:$h - 4px;
       background: #bfbfbf;
       border-radius: $h/2;
       position: relative;
-   > span{
-      position: absolute;
-      top: 2px;
-      left:2px;
-      height: $h2;
-      width: $h2;
-      background: white;
-      border-radius: $h2 / 2;
-      transition: all 250ms;
-    }
-  }
-    &.gulu-checked{
+     &.gulu-checked{
       background: #1890ff;
     >span {
       left: calc(100% - #{$h2} - 2px);
@@ -53,4 +42,16 @@ $h2:$h - 4px;
   &.gulu-checked:active{
     > span {width: $h2 + 4px; margin-left: -4px;}
   }
+   > span{
+      position: absolute;
+      top: 2px;
+      left:2px;
+      height: $h2;
+      width: $h2;
+      background: white;
+      border-radius: $h2 / 2;
+      transition: all 250ms;
+    }
+  }
+   
 </style>
--- gulu-ui/src/lib/Tabs.vue
+++ gulu-ui/src/lib/Tabs.vue
@@ -1,28 +1,83 @@
 <template>
-   <div>
-       tabs组件
-       <component is="defaults[0]"/>
-       <component is="defaults[1]"/>
+    <div class="gulu-tabs">
+    <div class="gulu-tabs-nav">  
+        <div class="gulu-tabs-nav-item "
+        v-for="(t,index) in titles" @click="select(t)" :class="{selected:t===selected}" :key="index">{{t}}</div>
+  </div>
+    <div class="gulu-tabs-content">
+    <component class="gulu-tabs-content-item" 
+    :class="{selected:c.props.title===selected}"
+    v-for=" c in defaults" :is="c"  />
+  </div>
    </div>
 </template>
 
 <script lang="ts">
+import { computed } from 'vue'
 import Tab from './Tab.vue'
 export default {
+    props:{
+        selected:{
+            type:String
+        }
+    },
     setup(props,context){
-       const defaults = context.slots.default()
+      const defaults = context.slots.default()
        defaults.forEach((tag)=>{
          if(tag.type !== Tab){
              throw new Error('tabs子标签必须是tab')
          } 
        })
-       return {
-           defaults
+       const current = computed(()=>{
+         return defaults.filter((tag)=>{
+           return tag.props.title === props.selected
+         })[0]
+       })
+       const titles = defaults.map((tag) =>{
+           return tag.props.title
+       })
+       const select = (title:string)=>{
+         context.emit('update:selected',title)
        }
+       return {
+           defaults,titles,current,select
+       }
+      
     }
 }
 </script>
 
-<style>
+<style lang="scss">
+ $blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+    
+    &-item {
+      display: none;
 
+    &.selected {
+      display: block;
+    }
+  }
+}
+}
 </style>
