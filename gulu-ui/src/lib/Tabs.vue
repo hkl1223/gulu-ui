b09100ee5eb3cff601f8a1caf803302752c87fbf
hukunling <1746530669@qq.com>
2020-12-14 17:13:20 +0800

    修改1

--- gulu-ui/src/lib/Tabs.vue
+++ gulu-ui/src/lib/Tabs.vue
@@ -3,7 +3,7 @@
     <div class="gulu-tabs-nav" ref="container">  
         <div class="gulu-tabs-nav-item "
         v-for="(t,index) in titles" 
-        :ref="el =>{ if (t===selected) selectedItem = el }"
+        :ref="el =>{ if (el) navItems[index] = el }"
         @click="select(t)" :class="{selected:t===selected}" :key="index">{{t}}</div>
         <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
   </div>
@@ -16,7 +16,7 @@
 </template>
 
 <script lang="ts">
-import { computed, onMounted, onUpdated, ref, watchEffect } from 'vue'
+import { computed, onMounted, onUpdated, ref } from 'vue'
 import Tab from './Tab.vue'
 export default {
     props:{
@@ -25,20 +25,21 @@ export default {
         }
     },
     setup(props,context){
+      const navItems  = ref<HTMLDivElement[]>([])
       const indicator = ref<HTMLDivElement>(null)
       const container = ref<HTMLDivElement>(null)
-      const selectedItem =ref<HTMLDivElement>(null)
       const x = ()=>{
-      const {width} = selectedItem.value.getBoundingClientRect()
+       const divs = navItems.value
+       const result =divs.filter(div=>div.classList.contains('selected'))[0]
+      const {width} =result.getBoundingClientRect()
       indicator.value.style.width = width + 'px'
       const {left:left1} = container.value.getBoundingClientRect()
-      const {left:left2} = container.value.getBoundingClientRect()
-      const left = left2 - left1
+      const {left:left2} = result.getBoundingClientRect()
+      const left = left2 -left1
       indicator.value.style.left = left + 'px'
       }
-      
-      watchEffect(x)  
-
+      onMounted(x)
+      onUpdated(x)
       const defaults = context.slots.default()
        defaults.forEach((tag)=>{
          if(tag.type !== Tab){
@@ -61,7 +62,7 @@ export default {
            titles,
            current,
            select,
-           selectedItem,
+           navItems,
            indicator,
            container
        }
