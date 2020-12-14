2b7e4976be4572fb2acc1762a98762f3ea1ee1c8
hukunling <1746530669@qq.com>
2020-12-14 15:07:03 +0800

    使用css切换内容

--- gulu-ui/src/lib/Tabs.vue
+++ gulu-ui/src/lib/Tabs.vue
@@ -1,12 +1,25 @@
 <template>
    <div>
        tabs组件
+       <component is="defaults[0]"/>
+       <component is="defaults[1]"/>
    </div>
 </template>
 
-<script>
+<script lang="ts">
+import Tab from './Tab.vue'
 export default {
-
+    setup(props,context){
+       const defaults = context.slots.default()
+       defaults.forEach((tag)=>{
+         if(tag.type !== Tab){
+             throw new Error('tabs子标签必须是tab')
+         } 
+       })
+       return {
+           defaults
+       }
+    }
 }
 </script>
 
