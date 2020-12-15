25274923fe8d639a758862543833f5aa185e4782
hukunling <1746530669@qq.com>
2020-12-15 10:21:25 +0800

    高亮选中路由

--- gulu-ui/src/views/Doc.vue
+++ gulu-ui/src/views/Doc.vue
@@ -53,6 +53,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
 .layout {
   display: flex;
   flex-direction: column;
@@ -83,7 +84,7 @@ export default {
 aside {
   background: lightblue;
   width: 150px;
-  padding: 16px;
+  padding: 16px 0;
   position: fixed;
   top: 0;
   left: 0;
@@ -91,12 +92,21 @@ aside {
   height: 100%;
   >h2 {
     margin-bottom: 4px;
+    padding: 0 16px;
   }
  ol {
  li {
-      padding: 4px 0;
+    >a{
+      display: block;
+      padding: 4px 16px;
+      text-decoration: none;
     }
+  
+  .router-link-active{
+    background: white;
   }
+ }
+}
   main {
       overflow: auto;
   }
