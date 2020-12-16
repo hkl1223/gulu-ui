d62ea3bf7a9930604e1b03754ea21508ab393726
hukunling <1746530669@qq.com>
2020-12-16 17:15:55 +0800

    改造button

--- gulu-ui/src/components/SwitchDome.vue
+++ gulu-ui/src/components/SwitchDome.vue
@@ -31,13 +31,8 @@
 </template>
 
 <script lang="ts">
-import Switch from '../lib/Switch.vue'
-import Button from '../lib/Button.vue'
 import Switch1Demo from './Switch1.demo.vue'
 import Switch2Demo from './Switch2.demo.vue'
-import 'prismjs'
-import 'prismjs/themes/prism.css'
-const Prism = (window as any).Prism
 import Demo from './Dome.vue'
 
 import {
@@ -45,16 +40,12 @@ import {
 } from 'vue'
 export default {
   components: {
-    Button,
     Demo
   },
   setup() {
-    const bool = ref(false)
     return {
-      bool,
       Switch1Demo,
       Switch2Demo,
-      Prism
     }
   }
 }
