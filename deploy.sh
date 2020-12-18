51120af274a1999b573d6fd3422858f693046d31
hukunling <1746530669@qq.com>
2020-12-18 09:18:20 +0800

    修改地址

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M main &&
-git remote add origin https://github.com/hkl1223/gulu-ui-website.git &&
+git remote add origin https://github.com/hkl1223/gulu-ui.git &&
 git push -u origin main &&
 cd -
 echo hkl1223.github.io/gulu-ui-website/#/
